7fa4d8628c6399c10f58d5e631daf9a63a0fdef0
viktor <baranov.viktor.27@gmail.com>
2017-06-23 18:53:11 +0300

    New network ID

--- assets/javascripts/application/index.js
+++ assets/javascripts/application/index.js
@@ -40,7 +40,7 @@ function checkNetworkVersion(web3, cb) {
 	      swal("Warning", msgNotOracles, "warning");
 	      cb(false);
 	    }  break;
-	     case "8995": {
+	     case "12648430": {
 	       console.log('This is Oracles from Metamask');
 	       cb(true);
 	    }  break;
