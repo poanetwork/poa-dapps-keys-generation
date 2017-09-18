2b45a9d8f4a7a8da16f882f12a62338ff6b7da5a
viktor <baranov.viktor.27@gmail.com>
2017-09-18 12:32:11 -0700

    gas estimation correction; this closes #9

--- assets/javascripts/application/createKeys.js
+++ assets/javascripts/application/createKeys.js
@@ -1,5 +1,28 @@
-function createKeys(web3, func, keys, address, contractAddr, cb) {
-  var funcParamsNumber = 3;
+function createKeys(web3, keys, contractAddr, abi, cb) {
+  console.log("***Create keys function***");
+  attachToContract(web3, abi, contractAddr, function(err, oraclesContract) {
+    console.log("attach to oracles contract");
+    if (err) {
+      console.log(err)
+      return cb();
+    }
+
+    console.log(keys);
+    
+    oraclesContract.createKeys.sendTransaction(
+      "0x" + keys.miningKey.miningKeyObject.address, 
+      "0x" + keys.payoutKey.payoutKeyObject.address, 
+      "0x" + keys.votingKey.votingKeyObject.address,
+      function(err, txHash) {
+        if (err) {
+          cb(txHash, err);
+          return;
+        }
+        cb(txHash);
+    });
+  });
+
+  /*var funcParamsNumber = 3;
   var standardLength = 32;
 
   var parameterLocation = standardLength*funcParamsNumber;
@@ -24,5 +47,5 @@ function createKeys(web3, func, keys, address, contractAddr, cb) {
         });
       });
     });
-  });
+  });*/
 }
