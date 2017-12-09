fe98d6f5673c373244cb395081aa8cfabd2ff1ad
Roman Storm <rstormsf@gmail.com>
2017-12-09 12:45:05 -0800

    add event on close to prevent from accidencial closing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+window.addEventListener("beforeunload", function (event) {
+  event.returnValue = "Are you sure?";
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
