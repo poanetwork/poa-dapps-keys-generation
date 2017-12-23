9db8554c66fa6aa4b29958e632a22d600ddc2990
Igor Barinov <424628+igorbarinov@users.noreply.github.com>
2017-12-23 13:44:51 -0500

    Problem: Links pointer to /oracles-wiki repo and /oraclesorg/ organization Solution: replace to /wiki repo and /poanetwork/ orgaziation

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -1,6 +1,6 @@
 let errorMsgNoMetamaskAccount = `You haven't chosen any account in MetaMask.
 Please, choose your initial key in MetaMask and reload the page.
-Check POA Network <a href='https://github.com/oraclesorg/oracles-wiki' target='blank'>wiki</a> for more info.`;
+Check POA Network <a href='https://github.com/poanetwork/wiki' target='blank'>wiki</a> for more info.`;
 
 
 function generateElement(msg){
@@ -37,7 +37,7 @@ let getWeb3 = () => {
               netIdName = 'ERROR'
               errorMsg = `You aren't connected to POA Network. 
                   Please, switch on Oracles plugin and refresh the page. 
-                  Check POA Network <a href='https://github.com/oraclesorg/oracles-wiki' target='blank'>wiki</a> for more info.`
+                  Check POA Network <a href='https://github.com/poanetwork/wiki' target='blank'>wiki</a> for more info.`
               console.log('This is an unknown network.', netId)
           }
           results = {
